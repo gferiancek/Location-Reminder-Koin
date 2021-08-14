fdf83ffe7867e4466868b8f8547aa1bd390e28b6
gferiancek <gferiancek@gmail.com>
2021-08-14 11:04:49 -0400

    Initial commit feat: Initial Screen
    Reminders screen has been created with RecyclerView and a no data state.  Neccessary Navigation Dependencies included.

---
+++ app/src/main/java/com/example/locationreminder/presentation/ui/reminders_list/RemindersListViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.locationreminder.presentation.ui.reminders_list
+
+import androidx.lifecycle.ViewModel
+
+class RemindersListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
